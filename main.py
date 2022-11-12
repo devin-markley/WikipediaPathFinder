3eb4c47a54877d9ae6e9d40d3b65e710de3a994e
devin-markley <94972879+devin-markley@users.noreply.github.com>
2022-11-12 13:05:30 -0800

    Update and rename hello.py to main.py

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ from bs4 import BeautifulSoup
 
 startLink = input("Enter starting wikipedia link: ")
 endLink = input("Enter ending wikipedia link: ")
+listLinks = []
 
-def link(link):
+def linkfinder(listLinks, link):
+    #Connecting to the webpage
     resp = requests.get(link)
     if resp.status_code == 200:
         print("Successfully opened the webpage")
-        print("These are all the hypertext links on the page")
-
+        #Crawling through the webpage looking for links
         soup=BeautifulSoup(resp.text, 'html.parser')
-        #Code could be improved here by removing some of the areas it searchs for links
-        for link in soup.find_all('a'):
-            #instead of printing store the data in an array
-            print(link.get('href'))
+        for link in soup.find_all('a', href=True):
+            listLinks.append(link["href"])
     else:
         print("error")
 
-link(startLink)
-link(endLink)
+startLinkList = []
+linkfinder(startLinkList, startLink)
+
+print(startLinkList)
