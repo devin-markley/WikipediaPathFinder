4d6d1a9e1e99954ad15540ff81f4cd448f29e309
devin-markley <94972879+devin-markley@users.noreply.github.com>
2022-11-12 13:00:45 -0800

    Instead of printing each link individually they are stored in an array

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+startLink = input("Enter starting wikipedia link: ")
+endLink = input("Enter ending wikipedia link: ")
+listLinks = []
+
+def linkfinder(listLinks, link):
+    #Connecting to the webpage
+    resp = requests.get(link)
+    if resp.status_code == 200:
+        print("Successfully opened the webpage")
+        #Crawling through the webpage looking for links
+        soup=BeautifulSoup(resp.text, 'html.parser')
+        for link in soup.find_all('a', href=True):
+            listLinks.append(link["href"])
+    else:
+        print("error")
+
+startLinkList = []
+linkfinder(startLinkList, startLink)
+
+print(startLinkList)
+
+
+    
+
