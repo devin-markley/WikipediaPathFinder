34093a4bd8a047fcff52222c76c63196ce6e68da
devin-markley <94972879+devin-markley@users.noreply.github.com>
2022-11-16 12:50:07 -0800

    Narrowing scope of link scrapping function

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from bs4 import BeautifulSoup
 
 startLink = input("Enter starting wikipedia link: ")
 endLink = input("Enter ending wikipedia link: ")
-listLinks = []
 
+listLinks = []
+#TODO set a limit to depth
+#TODO if endlink is found function should exit the program
 def linkfinder(listLinks, link):
     #Connecting to the webpage
     resp = requests.get(link)
@@ -12,16 +14,26 @@ def linkfinder(listLinks, link):
         print("Successfully opened the webpage")
         #Crawling through the webpage looking for links
         soup=BeautifulSoup(resp.text, 'html.parser')
+        #Limiting the list to only links that lead to other wikipedia webpages
         for link in soup.find_all('a', href=True):
-            listLinks.append(link["href"])
+            if str(link["href"]).startswith("/wiki/"):
+                newLink = link["href"]
+                if not '/wiki/' in newLink:
+                    if '#' in newLink:
+                        indexOfHashtag = newLink.find('#')
+                    newLink = newLink[:indexOfHashtag]
+                    if ':' in newLink:
+                        continue
+                listLinks.append("https://en.wikipedia.org" + newLink)
+                #TODO Check if endlink is on page called by link
+                #if endLink in childernLinkList:
+                #print(f"A link too {endLink} is on the page {startLink}")
     else:
         print("error")
 
-startLinkList = []
-linkfinder(startLinkList, startLink)
+childernLinkList = []
+linkfinder(childernLinkList, startLink)
 
-print(startLinkList)
-
-
-    
-
+    #for links in  childernLinkList:
+        #TODO find all links contained on childernLink pages
+        #TODO save them in an array named after there parent link
