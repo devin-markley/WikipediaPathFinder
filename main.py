d30c7bd2f16d606350dad8734e1eba17827e82ea
devin-markley <94972879+devin-markley@users.noreply.github.com>
2022-12-28 09:08:30 -0800

    Adding database for successful paths

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from bs4 import BeautifulSoup
 import grequests
 import time
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client["mydatabase"]
+
+cache_collection = db["cache"]
+
+def get_data_from_cache(key):
+    cache_entry = cache_collection.find_one({"key": key})
+    if cache_entry is not None:
+        return cache_entry["data"]
+    return None
+
+def store_data_in_cache(key, data):
+    cache_entry = {"key": key, "data": data}
+    cache_collection.insert_one(cache_entry)
 
 startLink = "https://en.wikipedia.org/wiki/Latte"
-endLink = "/wiki/United"
+endLink = "/wiki/Merienda"
 
-def asyncLinkFinder(listLinks, webpageToSearch, parentList):
-    #Connecting to the webpage
-    #TODO Make sure Linkfinder doesn't search the same link twice
+def LinkFinder(listLinks, webpageToSearch, parentList): 
     batches = 20
     page = grequests.get(webpageToSearch)
     response = grequests.map([page], size=batches)
@@ -16,7 +30,6 @@ def asyncLinkFinder(listLinks, webpageToSearch, parentList):
             soup=BeautifulSoup(res.text, 'html.parser')
         except AttributeError:
             continue
-        #TODO Limit links searched by linkfinder to a greater degree
         for link in soup.find_all('a', href=True):
             if str(link["href"]).startswith("/wiki/"):
                 newLink = link["href"]
@@ -35,7 +48,8 @@ def asyncLinkFinder(listLinks, webpageToSearch, parentList):
                 newParentList = [webpageToSearch] + parentList
                 if len(newParentList) < 5:
                     print(newLink)
-                    listLinks.append(("https://en.wikipedia.org" + newLink, newParentList))    
+                    listLinks.append(("https://en.wikipedia.org" + newLink, newParentList))
+                    store_data_in_cache(newLink, listLinks[-1:])    
                 else:
                     print("Path could not be found")
                     print(listLinks)
@@ -48,13 +62,17 @@ explorationQueue = [(startLink, [])]
 alreadySearched = []
 startTime= time.time()
 
-while(explorationQueue):
-    i = explorationQueue.pop(0)
-    if(asyncLinkFinder(explorationQueue, i[0], i[1])):
-        endTime = time.time()
-        elapsedTime = endTime - startTime
-        print(elapsedTime)
-        print("this was the path to the endlink" + " " + str(i[0])+ " " + str(i[1]))
-        break  
-    alreadySearched.append(i[0])
-quit()
+data = get_data_from_cache(endLink)
+if data is not None:
+    print(data)
+else:
+    while(explorationQueue):
+        i = explorationQueue.pop(0)
+        if(LinkFinder(explorationQueue, i[0], i[1])):
+            endTime = time.time()
+            elapsedTime = endTime - startTime
+            print(elapsedTime)
+            print("this was the path to the endlink" + " " + str(i[0])+ " " + str(i[1]))
+            break  
+        alreadySearched.append(i[0])
+    quit()
