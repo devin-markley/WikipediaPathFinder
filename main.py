e5db2228ed755a17b4bf48e61e44b6eb8e8d2082
devin-markley <94972879+devin-markley@users.noreply.github.com>
2023-01-11 17:47:19 -0800

    Cache built for known link paths and webpages already searched

--- main.py
+++ main.py
@@ -6,66 +6,95 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client["mydatabase"]
 
-cache_collection = db["cache"]
+knownPathCollection = db["endLinkPaths"]
+webpageLinkCollection = db["webpageLinks"]
 
-def get_data_from_cache(key):
-    cache_entry = cache_collection.find_one({"key": key})
+def storingEndlinkPaths(endLink, path, startLink):
+    knownPathCollection.update_one({"key": endLink}, {"$set": {startLink: path}}, upsert= True)
+
+#TODO Change name for key
+def gettingEndLinkPaths(key, startLink):
+    cache_entry = knownPathCollection.find_one({"Path": key})
+    if cache_entry is not None:
+        try:
+            return cache_entry[startLink]
+        except KeyError:
+            return None
+    return None
+
+def accessingCachedLinksOn(webpage):
+    cache_entry = webpageLinkCollection.find_one({"key": webpage})
     if cache_entry is not None:
         return cache_entry["data"]
     return None
 
-def store_data_in_cache(key, data):
-    cache_entry = {"key": key, "data": data}
-    cache_collection.insert_one(cache_entry)
+def cacheLinksConatinedOn(webpage, linksOn):
+    webpageLinkCollection.insert_one({"key": webpage, "data": linksOn})
 
-startLink = "https://en.wikipedia.org/wiki/Latte"
-endLink = "/wiki/Merienda"
+startLink = "/wiki/Pizza"
+endLink = "/wiki/Food"
 
-def LinkFinder(listLinks, webpageToSearch, parentList): 
-    batches = 20
-    page = grequests.get(webpageToSearch)
-    response = grequests.map([page], size=batches)
-    for res in response:
-        try:
-            soup=BeautifulSoup(res.text, 'html.parser')
-        except AttributeError:
-            continue
-        for link in soup.find_all('a', href=True):
-            if str(link["href"]).startswith("/wiki/"):
-                newLink = link["href"]
-                if not '/wiki/' in newLink:
-                    if '#' in newLink:
-                        indexOfHashtag = newLink.find('#')
-                    newLink = newLink[:indexOfHashtag]
-                if ':' in newLink:
-                    continue
-                if 'Main_Page' in newLink:
-                    continue
-                if 'identifier' in newLink:
-                    continue
-                if "https://en.wikipedia.org" + newLink in alreadySearched or [item for item in listLinks if item[0] =="https://en.wikipedia.org" + newLink]:
-                    continue
-                newParentList = [webpageToSearch] + parentList
-                if len(newParentList) < 5:
-                    print(newLink)
-                    listLinks.append(("https://en.wikipedia.org" + newLink, newParentList))
-                    store_data_in_cache(newLink, listLinks[-1:])    
-                else:
-                    print("Path could not be found")
-                    print(listLinks)
-                    quit()
-                if (newLink == endLink):
-                    return True
+def LinkFinder(listLinks, webpageToSearch, parentList):
+    pathFoundFlag = 0 
+    knownLinks = accessingCachedLinksOn(webpageToSearch)
+    if knownLinks is not None:
+        if len(parentList) < 5:
+            newParentList = [webpageToSearch] + parentList
+            for link in knownLinks:
+                listLinks.append((link, newParentList)) 
+        else:
+            print("Path could not be found")
+            quit()  
+    else:
+        batches = 20
+        page = grequests.get("https://en.wikipedia.org" + webpageToSearch)
+        response = grequests.map([page], size=batches)
+        linksOnWebpageToSearch = []
+        for res in response:
+            try:
+                soup=BeautifulSoup(res.text, 'html.parser')
+            except AttributeError:
+                continue
+            for link in soup.find_all('a', href=True):
+                if str(link["href"]).startswith("/wiki/"):
+                    newLink = link["href"]
+                    if not '/wiki/' in newLink:
+                        if '#' in newLink:
+                            indexOfHashtag = newLink.find('#')
+                        newLink = newLink[:indexOfHashtag]
+                    if ':' in newLink:
+                        continue
+                    if 'Main_Page' in newLink:
+                        continue
+                    if 'identifier' in newLink:
+                        continue
+                    linksOnWebpageToSearch.append(newLink)
+                    if newLink in alreadySearched or [item for item in listLinks if item[0] ==  newLink]:
+                        continue
+                    newParentList = [webpageToSearch] + parentList
+                    if len(newParentList) < 5:
+                        listLinks.append((newLink, newParentList))
+                        storingEndlinkPaths(newLink, listLinks[-1:], startLink)    
+                    else:
+                        print("Path could not be found")
+                        quit()
+                    if (newLink == endLink):
+                        pathFoundFlag = 1
+        cacheLinksConatinedOn(webpageToSearch, linksOnWebpageToSearch)
+        linksOnWebpageToSearch.clear()
+        if (pathFoundFlag == 1):
+            return True
     return False
 
 explorationQueue = [(startLink, [])]
 alreadySearched = []
 startTime= time.time()
 
-data = get_data_from_cache(endLink)
+data = gettingEndLinkPaths(endLink, startLink)
 if data is not None:
     print(data)
 else:
+    print("No known paths currently searching")
     while(explorationQueue):
         i = explorationQueue.pop(0)
         if(LinkFinder(explorationQueue, i[0], i[1])):
